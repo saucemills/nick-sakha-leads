11820860997382683a54ee346dfeb8eee7966d63
Jon Miller <jonmiller@Jons-MacBook-Pro.local>
2022-08-25 14:26:17 -0700

    remove the allstate logo and change bg/button color

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,8 @@ import allstateLogo from "../public/allstate.png";
 
 function Header() {
   return (
-    <div className="bg-blue-900 flex w-full">
-      <div className="max-w-6xl px-5 py-2">
-        <Image width={200} height={44} src={allstateLogo} />
-      </div>
+    <div className="bg-[#042C64] h-10 flex w-full">
+      <div className="max-w-6xl px-5 py-2"></div>
     </div>
   );
 }
